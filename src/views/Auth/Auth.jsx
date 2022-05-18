531449b67894ae672337cce3620e89904e769567
Trevor Rezac <TrevorRezac@TrevsMBP.local>
2022-05-17 17:51:23 -0700

    created signUp form, created useFrom context

--- src/views/Auth/Auth.jsx
+++ src/views/Auth/Auth.jsx
@@ -1,2 +1,27 @@
-import { Redirect, useHistory } from 'react-router-dom';
-import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { useUserContext } from '../../context/UserContext'
+import { useForm } from '../../hooks/useForm';
+
+export default function Auth() {
+  const { signUp } = useUserContext();
+  const { formState, handleChange } = useForm({ email: '', password: ''});
+
+  async function handleSignUp() {
+    await signUp(formState.email, formState.password)
+  }
+  
+  return (
+    <>
+    <form onSubmit={handleSignUp}>
+      <input 
+      onChange={handleChange}
+      placeholder='Email'/>
+      <input 
+      onChange={handleChange}
+      placeholder='Password'/>
+      <button>Sign Up</button>
+    </form>
+    </>
+  )
+}
+
